2ad8a8c1afdaa0d33c93be4d6abf1c8dcc0ad71c
HKobe <HKobeissi1@hotmail.com>
2016-07-06 13:21:00 -0400

    Initial Commit

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Hassan Kobeissi
+ * Created July 7th, 2016
+ * Modified on July 7th, 2016
+ * Program intended to display dice rolls and tallies.
+ */
 namespace Assignment4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ///Psuedo Number Objbect
+            Random random = new Random();
+             ///New List named diceRolls of type int
+            List<int> diceRolls = new List<int>();
+
+            int[] tally = new int[13];
+            ///Maximum number of rolls
+            int maxRolls = 36000;
+
+
+            //builds the list 
+            for (int index = 0; index < maxRolls; index++)
+            {
+                ///Dice One = Random Rolls from 1-6
+                int firstDie = random.Next(1, 7);
+                int secondDie = random.Next(1, 7);
+                ///Sum of the two values
+                int dice = firstDie + secondDie;
+                /// the sum in place and incremented in a tally
+                tally[dice]++;
+                diceRolls.Add(dice);
+            }
+
+            ///Sorts List
+            diceRolls.Sort();
+
+
+            // display list 
+            /* 
+            foreach (int result in diceRolls) 
+            { 
+                Console.Write(result + " "); 
+            } 
+            Console.WriteLine(); 
+            */
+
+
+            for (int index = 2; index < 13; index++)
+            {
+                Console.WriteLine("Roll # {0} was rolled {1} times.", index, tally[index]);
+            }
+
         }
     }
 }
