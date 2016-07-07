f5474600c0184de3d9fd5dcd8af6e5bc48ad2f16
HKobe <HKobeissi1@hotmail.com>
2016-07-06 21:37:41 -0400

    Airlines Reservation Menu updated, switch and while updated

---
+++ AirlineReservationSystem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMenu();
+            
+    }
+        #region  Airline Menu
+
+        static public void ShowMenu()
+        {
+            int menuchoice = 0;
+            while (menuchoice != 3)
+            {
+
+                Console.WriteLine("~~~~~~~Airline Reservation System~~~~~~~~~");
+                Console.WriteLine("Please select your method of travel.");
+                Console.WriteLine("        1. First Class");
+                Console.WriteLine("        2. Seond Class");
+                Console.WriteLine("        3. Exit");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.Write("            Enter Choice : ");
+                menuchoice = int.Parse(Console.ReadLine());
+
+                switch (menuchoice)
+                {
+                    case 1:
+                        Console.WriteLine("Seat 5");
+                        break;
+                    case 2:
+                        Console.WriteLine("Seat 8");
+                        break;
+                    case 3:
+                        break;                                        
+                    default:
+                        throw new ArgumentOutOfRangeException("Unknown value");
+                }
+            }
+        }
+        #endregion
+
+    }
+}
--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -18,7 +18,7 @@ namespace Assignment4
             #region Dice 
             ///Psuedo Number Objbect
             Random random = new Random();
-             ///New List named diceRolls of type int
+            ///New List named diceRolls of type int
             List<int> diceRolls = new List<int>();
 
             int[] tally = new int[13];
@@ -44,67 +44,21 @@ namespace Assignment4
 
 
             // display list 
-            /* 
-            foreach (int result in diceRolls) 
-            { 
-                Console.Write(result + " "); 
-            } 
-            Console.WriteLine(); 
-            */
+
 
             //A minimum of two can be rolled
             for (int index = 2; index < 13; index++)
             {
                 Console.WriteLine("Roll # {0} was rolled {1} times.", index, tally[index]);
             }
-            #endregion           
-            Console.WriteLine();
-            ShowMenu();       
-            }
-
-        #region  Airline Menu
-
-        static public int ShowMenu()
-        {
-            Console.WriteLine(" THANK YOUR FOR FLYING AIRCANADA");
-            Console.WriteLine("=================================");
-            Console.WriteLine("    Enter 1 for First Class   ");
-            Console.WriteLine("    Enter 2 for Economy       ");
-            Console.WriteLine("    Enter 3 to Exit Menu      ");
-            Console.WriteLine("=================================");
-                Console.Write("     Enter Your Option : ");
-            var userInput = Console.ReadLine();
-            try
-            {
-
-            }
-            catch
-            {
-
-            }
-            return Convert.ToInt32(userInput);
             #endregion
 
-            /*string choice = "x";
-           switch (choice)
-           {
-               case "1":
-                   Console.WriteLine("First Class");
-                   break;
-               case "2":
-                   Console.WriteLine("Econonmy");
-                   break;
-               case "3":
-                   Console.WriteLine("Exit");
-                   break;
-               default:
-                   Console.WriteLine("Error");
-                   break;*/
-
 
         }
-    }
+           
+                 }
+             }
        
 
-        }
+        
 
